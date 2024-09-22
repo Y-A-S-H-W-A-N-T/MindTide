07a7cf77316cf3f5fa615cd0358621119a2d9250
raoyashwant <raoyashwant132@gmail.com>
2024-09-22 05:42:08 +0530

    API Route

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,19 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['uspoloassn.in'],
-    remotePatterns: [
+    "remotePatterns": [
       {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/**"
+      }
+    ]
   },
-  async rewrites() {
+  async headers() {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://mind-tide.vercel.app/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Content-Type, Authorization' },
+        ],
       },
     ];
   },
